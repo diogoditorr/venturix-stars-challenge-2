b68c94a1be834a3274d3c69ac320474c517e313d
Diego Oliveira <35296262+diogoditorr@users.noreply.github.com>
2022-08-03 17:59:55 -0300

    feat: add responsive design

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,21 @@ const numberOfPeopleInput = document.querySelector("input#numberOfPeople");
 const resetButton = document.querySelector('button[type="reset"]');
 const tipAmount = document.querySelector("#tipAmount");
 const totalAmount = document.querySelector("#totalAmount");
+const labelError = document.querySelector(".tip-calc__field__label__error");
 
 function calculate() {
     console.log(Object.fromEntries(new FormData(form).entries()));
-    let { bill, numberOfPeople, tipPorcent, tipPorcentCustom } =
-        Object.fromEntries(new FormData(form).entries());
+
+    const formData = new FormData(form);
+    const bill = formatCurrencyToNumber(formData.get("bill"));
+    const numberOfPeople = Number(formData.get("numberOfPeople"));
+    const tipPorcent = Number(formData.get("tipPorcent"));
+    const tipPorcentCustom = Number(formData.get("tipPorcentCustom"));
 
     if (!bill || !numberOfPeople || (!tipPorcent && !tipPorcentCustom)) {
-        return;
+        return clearResults();
     }
 
-    bill = formatCurrencyToNumber(bill);
     const tip = tipPorcent || tipPorcentCustom;
     const total = bill * (1 + tip / 100);
     const tipAmountPerPerson = ((bill * (tip / 100)) / numberOfPeople).toFixed(
@@ -28,9 +32,25 @@ function calculate() {
     totalAmount.innerHTML = `$${totalAmountPerPerson}`;
 }
 
+function clearResults() {
+    tipAmount.innerHTML = "$0.00";
+    totalAmount.innerHTML = "$0.00";
+}
+
 function onInputChange() {
     resetButton.disabled = false;
     calculate();
+}
+
+function formatCurrencyToNumber(value) {
+    return Number(value.replace(/[^0-9.]+/g, ""));
+}
+
+function formatNumberToCurrency(value) {
+    return value.toLocaleString("en-US", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
 }
 
 customTipInput.addEventListener("change", () => {
@@ -45,33 +65,33 @@ selectableTipsInputs.forEach((tip) => {
     });
 });
 
-function formatCurrencyToNumber(value) {
-    return Number(value.replace(/[^0-9.]+/g, ""));
-}
-
-function formatNumberToCurrency(value) {
-    return value.toLocaleString("en-US", {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-    });
-}
-
-billInput.addEventListener("change", ({ target }) => {
-    if (!Number.isNaN(Number(target.value))) onInputChange();
-});
+billInput.addEventListener("change", onInputChange);
 billInput.addEventListener("focus", ({ target }) => {
     const value = target.value;
     target.value = formatCurrencyToNumber(value);
+    target.select();
 });
 billInput.addEventListener("blur", ({ target }) => {
     const value = Number(target.value) || 0;
     target.value = formatNumberToCurrency(value);
 });
+
 numberOfPeopleInput.addEventListener("change", ({ target }) => {
-    if (Number(target.value) !== 0) onInputChange();
+    const value = Number(target.value);
+    if (value === 0) {
+        target.classList.add("error")
+        labelError.textContent = "Can't be zero";
+    } else {
+        target.classList.remove("error");
+        labelError.textContent = "";   
+    }
+
+    target.value = Math.floor(value);
+    onInputChange();
 });
 
 resetButton.addEventListener("click", () => {
     resetButton.disabled = true;
+    clearResults();
     form.reset();
 });
